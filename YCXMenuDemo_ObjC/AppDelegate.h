63546e76df47d289a221bdf7cb38d2d849bd7dbb
aster0id <aster0id@sina.com>
2015-05-06 09:29:46 +0800

    init

---
+++ YCXMenuDemo_ObjC/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  YCXMenuDemo_ObjC
+//
+//  Created by 牛萌 on 15/5/6.
+//  Copyright (c) 2015年 NiuMeng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ YCXMenuDemo_ObjC/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  YCXMenuDemo_ObjC
+//
+//  Created by 牛萌 on 15/5/6.
+//  Copyright (c) 2015年 NiuMeng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
