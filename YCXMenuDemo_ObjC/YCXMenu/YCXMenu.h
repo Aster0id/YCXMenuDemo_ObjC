bb3ba0a378cbecf5c202bda0632b9eaafca03841
aster0id <aster0id@sina.com>
2015-05-06 09:47:40 +0800

    添加YCXMenu文件

---
+++ YCXMenuDemo_ObjC/YCXMenu/YCXMenu.h
@@ -0,0 +1,13 @@
+//
+//  YCXMenu.h
+//  YCXMenuDemo_ObjC
+//
+//  Created by 牛萌 on 15/5/6.
+//  Copyright (c) 2015年 NiuMeng. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface YCXMenu : NSObject
+
+@end
---
+++ YCXMenuDemo_ObjC/YCXMenu/YCXMenu.m
@@ -0,0 +1,13 @@
+//
+//  YCXMenu.m
+//  YCXMenuDemo_ObjC
+//
+//  Created by 牛萌 on 15/5/6.
+//  Copyright (c) 2015年 NiuMeng. All rights reserved.
+//
+
+#import "YCXMenu.h"
+
+@implementation YCXMenu
+
+@end
